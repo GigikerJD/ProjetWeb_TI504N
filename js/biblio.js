81be85e952f02379eafd4e330ac4b459039706cc
GigikerJD <nelgie.ferrol94@gmail.com>
2023-01-06 09:40:30 +0100

    finition javascript

--- js/biblio.js
+++ js/biblio.js
@@ -4,25 +4,28 @@ function addMusician(){
     let instrus = document.getElementById("instrus").value;
 
     let table = document.getElementById("librairies");
+
+    if(name == "" || firstname == ""){
+        alert("Please fill in the form !");
+    }
+    else{
+        let newRow = table.insertRow(table.rows.length);
+        let cell = newRow.insertCell(0);
+        let cell1 = newRow.insertCell(1);
+        let cell2 = newRow.insertCell(2);
+        cell.innerHTML = name;
+        cell1.innerHTML = firstname;
+        cell2.innerHTML = instrus;
     
-    let newRow = table.insertRow(table.length);
-    var cell = newRow.insertCell(0);
-    var cell1 = newRow.insertCell(1);
-    var cell2 = newRow.insertCell(2);
-
-    cell.innerHTML = name;
-    cell1.innerHTML = firstname;
-    cell2.innerHTML = instrus;
-
-    cell.style.border = "2px ridge rgb(67, 59, 59)";
-    cell1.style.border = "2px ridge rgb(67, 59, 59)";
-    cell2.style.border = "2px ridge rgb(67, 59, 59)";
+        cell.style.border = "2px ridge rgb(67, 59, 59)";
+        cell1.style.border = "2px ridge rgb(67, 59, 59)";
+        cell2.style.border = "2px ridge rgb(67, 59, 59)";
+        
     
-
-    let cpt = 0; //on incrémente le compteur
-    cpt = table.rows.length - 1;
-
-    let result = document.getElementById("result");
-    result.innerHTML = "<p>Vous avez ${cpt} musiciens ajoutées</p>";
-
+        let cpt = 0; //on incrémente le compteur
+        cpt = table.rows.length - 1;
+    
+        let result = document.getElementById("result");
+        result.innerHTML = "Vous avez " + cpt + " musiciens ajouté(s)";
+    }
 }
