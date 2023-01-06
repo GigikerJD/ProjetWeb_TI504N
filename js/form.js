f327e688b9e769366cb30e6e2a291ede80292474
GigikerJD <nelgie.ferrol94@gmail.com>
2023-01-06 15:45:36 +0100

    update javascritpt

--- js/form.js
+++ js/form.js
@@ -8,3 +8,22 @@ function showAccount(){
         create.style.display == "flex";
     }
 }
+
+
+function Login(){
+    let loginForm = document.get
+
+    if(loginForm[0].value == "" || loginForm[1] == ""){
+        alert("Please, you need to fill your email and password !");
+    }
+    else if(loginForm[0].value == "" && loginForm[1].value != ""){
+        alert("Your email is missing !");
+    }
+    else if(loginForm[0].value != "" && loginForm[1].value == ""){
+        alert("Your password is missing !");
+    }
+    else{
+        let pra = document.getElementById("username");
+        pra.innerHTML = "Bonjour " + loginForm[0];
+    }
+}
