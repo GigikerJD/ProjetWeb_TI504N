d2858a3afc71a1a1ff1cd36558b5f55c8a36998f
GigikerJD <nelgie.ferrol94@gmail.com>
2023-01-06 08:48:51 +0100

    correction d'ajout d'éléments

--- js/biblio.js
+++ js/biblio.js
@@ -1,7 +1,6 @@
 function addMusician(){
     let name = document.getElementById("name").value;
     let firstname = document.getElementById("fname").value;
-    let age = document.getElementById("age").value;
     let instrus = document.getElementById("instrus").value;
 
     let table = document.getElementById("librairies");
@@ -15,10 +14,15 @@ function addMusician(){
     cell1.innerHTML = firstname;
     cell2.innerHTML = instrus;
 
+    cell.style.border = "2px ridge rgb(67, 59, 59)";
+    cell1.style.border = "2px ridge rgb(67, 59, 59)";
+    cell2.style.border = "2px ridge rgb(67, 59, 59)";
+    
+
     let cpt = 0; //on incrémente le compteur
     cpt = table.rows.length - 1;
 
     let result = document.getElementById("result");
-    result.innerText = "Vous avez " + cpt + " musiciens ajoutées";
+    result.innerHTML = "<p>Vous avez ${cpt} musiciens ajoutées</p>";
 
 }
